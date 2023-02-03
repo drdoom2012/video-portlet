0118d1868af17d7671139a37c892b169525e0f83
extcizekma <m.cizek@external.t-mobile.cz>
2023-02-03 14:51:03 +0100

    video remove

--- public/video-a/resources/js/player/streamTypeController.js
+++
@@ -1,8 +0,0 @@
-var playerType = getPlayerType();
-var isFlash = !(playerType == "html5Dash" || playerType == "html5Hls");
-
-if (isFlash) {
-	initFlash("mhmp-hls.json");
-}
-
-hideElement(isFlash);
